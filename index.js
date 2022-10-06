765fb554971ec565ef51b3a9f85ac319852455f1
HarshilGem <jetaniharshil0810@gmail.com>
2022-10-06 15:57:53 +0530

    api change

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
-const mongoose  = require('mongoose')
+const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://HarshilJetani:987654321@cluster0.bq3ljih.mongodb.net/AppDB?retryWrites=true&w=majority");
 const app = express();
-app.route("/").get((req,resp)=>{                        
-    resp.json("first api")
-});
+// app.route("/").get((req,resp)=>{                        
+//     resp.json("first api")
+// });
 
 const connection = mongoose.connection;
-connection.once("open",()=>{ 
+connection.once("open", () => {
     console.log("connected");
 });
 
 //middleware
 app.use(express.json());
-const userRoute= require("./routes/user");
-app.use("/user",userRoute );
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
+app.get('/', (req, resp) => {
+    resp.json("first api")
+})
 
 
-
-app.listen(5000,()=>console.log("server is running on 5000")); 
+app.listen(5000, () => console.log("server is running on 5000")); 
